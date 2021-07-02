c9570a0e79fa615b2a9f5060a388813416c54d21
mydogspies <mydogspies@zoho.com>
2021-07-02 19:24:19 +0200

    Fixed network connection error to api

--- components/status-wrapper.js
+++ components/status-wrapper.js
@@ -16,7 +16,7 @@ export default function StatusWrapper({children}) {
     async function getSite() {
 
         try {
-            const response = await fetch('http://10.110.0.3:5011/api/v1/status');
+            const response = await fetch('https://10.110.0.3:5011/api/v1/status');
             const res = response.json();
             setSite(res.online ? 'online' : 'offline');
             setOnlineStatus(res.online);
