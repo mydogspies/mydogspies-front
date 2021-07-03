d1683df89252a80ca9cbb89a0a7212c6cd22fc57
mydogspies <mydogspies@zoho.com>
2021-07-03 11:57:49 +0200

    Fixed api logic errors

--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,8 @@ MyApp.getInitialProps = async (appContext) => {
 // check if the on/offline flag is set in the db
 async function getSiteStatus() {
     try {
-        const res = await fetch(process.env.DB_API + '/api/v1/status');
+        // const res = await fetch(process.env.DB_API + '/api/v1/status');
+        const res = await fetch('https://api.mydogspies.com/api/v1/status');
         const status = await res.json();
         return status.online;
     } catch (error) {
