4ef4c4f4b9cb37a5110711a07086cbfaf622a936
mydogspies <mydogspies@zoho.com>
2021-07-07 14:32:54 +0200

    Fixed path error for image in the offline component

--- components/site-offline-component/site-offline-component.jsx
+++ components/site-offline-component/site-offline-component.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styled from "styled-components";
 import Image from 'next/image';
+import logo from '../../public/assets/dog_blue_final.svg';
 
 const OfflinePage = () => {
 
@@ -11,7 +12,7 @@ const OfflinePage = () => {
                 <title>Mydogspies.com - Coding, 3D and project management - Full stack development</title>
             </Head>
             <Grid>
-                <Image src="/../public/assets/dog_blue_final.svg" height={200} width={200} />
+                <Image src={logo} height={200} width={200} />
 
                 <Text>This site is under some serious construction and will be back end of summer 2021!</Text>
             </Grid>
