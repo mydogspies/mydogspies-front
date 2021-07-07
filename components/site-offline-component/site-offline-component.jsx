bad8299310c3065fd92805117828ddde4123e7cb
mydogspies <mydogspies@zoho.com>
2021-07-07 14:51:43 +0200

    Fixed path error for image in the offline component by adding package and adding next.config.js file

--- components/site-offline-component/site-offline-component.jsx
+++ components/site-offline-component/site-offline-component.jsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styled from "styled-components";
-import Image from 'next/image';
 import logo from '../../public/assets/dog_blue_final.svg';
 
 const OfflinePage = () => {
@@ -12,8 +11,8 @@ const OfflinePage = () => {
                 <title>Mydogspies.com - Coding, 3D and project management - Full stack development</title>
             </Head>
             <Grid>
-                <Image src={logo} height={200} width={200} />
 
+                <img src={logo} />
                 <Text>This site is under some serious construction and will be back end of summer 2021!</Text>
             </Grid>
 
