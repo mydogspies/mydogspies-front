089ecaa588499c4583bad0619cd7bc95b2370b07
mydogspies <mydogspies@zoho.com>
2021-07-11 14:33:34 +0200

    Updated dependencies

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,8 +8,7 @@ const options = {
             clientId: process.env.OKTA_CLIENT_ID,
             domain: process.env.OKTA_DOMAIN,
             scope: 'openid profile email',
-            authorizationUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/authorize/?response_type=code`,
-            state: false,
+            authorizationUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/authorize/?response_type=code&state=false`,
             protection: 'pkce',
             jwt: {
                 signingKey: process.env.JWT_SIGNING_KEY,
