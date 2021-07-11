d00482a5f6276c430bdf8fc288b4c764e0f1361e
mydogspies <mydogspies@zoho.com>
2021-07-11 16:52:11 +0200

    Fixing Okta errors

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,6 +11,7 @@ const options = {
             accessTokenUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/token`,
             authorizationUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/authorize/?response_type=code&state=false`,
             protection: 'pkce',
+            debug: true,
 
         }),
         // ...add more providers here
