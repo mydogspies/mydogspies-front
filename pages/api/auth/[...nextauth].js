1c1325845b23cb791c2ccb1cb35ecb095b1cda4b
mydogspies <mydogspies@zoho.com>
2021-07-11 19:19:30 +0200

    Fixing Okta errors

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,11 +5,18 @@ const options = {
     // Configure one or more authentication providers
     providers: [
         Providers.Okta({
+            // clientId: process.env.OKTA_CLIENT_ID,
+            // domain: process.env.OKTA_DOMAIN,
+            // scope: 'openid profile email',
+            // accessTokenUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/token`,
+            // authorizationUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/authorize/?response_type=code&state=false`,
+            // protection: 'pkce',
+            // debug: true,
+
             clientId: process.env.OKTA_CLIENT_ID,
-            domain: process.env.OKTA_DOMAIN,
+            issuer: `https://${process.env.OKTA_DOMAIN}/oauth2/default`,
+            redirectUri: 'https://mydogspies.com/api/auth/callback/okta',
             scope: 'openid profile email',
-            accessTokenUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/token`,
-            authorizationUrl: `https://${process.env.OKTA_DOMAIN}/oauth2/default/v1/authorize/?response_type=code&state=false`,
             protection: 'pkce',
             debug: true,
 
