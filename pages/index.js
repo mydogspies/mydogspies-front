c5f45aac070b6a42fea9446e847ee38b76734f48
mydogspies <mydogspies@zoho.com>
2021-07-14 13:45:52 +0200

    Added index page definition to globals.scss and refactored index.js page for that

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import Head from 'next/head';
+import styled from "styled-components";
+
 import OfflinePage from '../components/offline/site-offline-component/site-offline-component';
 
 export default function Home({status, userStatus}) {
 
     if (status || userStatus) {
         return (
-            <>
+            <Container className="index-page">
                 <Head>
                     <title>Mydogspies.com - Coding, 3D and project management - Full stack development</title>
                 </Head>
-                <main>
-                    <h1>Welcome to Mydogspies</h1>
+                <Temporary>
+                    <h2>Welcome to Mydogspies</h2>
                     <p>This is the index page.</p>
-                </main>
-            </>
+                </Temporary>
+            </Container>
         );
     } else {
         return (
@@ -23,5 +25,19 @@ export default function Home({status, userStatus}) {
         )
     }
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+`;
+
+// TODO temporary - should be removed asap
+const Temporary = styled.div`
+    text-align: center;
+  position: relative;
+  bottom: 3%;
+`;
 
 
